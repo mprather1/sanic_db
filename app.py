fa0b8260db5fa28afc10df89aa7237536babb0cd
Michael Prather <mprather1@gmail.com>
2017-06-14 18:24:31 +0000

    remaining routes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from sanic import Sanic
-import ssl
 from sanic.response import json
 from sanic.response import text
-import os
 import asyncio
 import aiopg
 import uvloop
+import os
+# import ssl
 
 database_name = os.environ['DATABASE_NAME']
 database_host = os.environ['DATABASE_HOST']
@@ -16,38 +16,67 @@ connection = 'postgres://{0}:{1}@{2}/{3}'.format(database_user,
                                                  database_password, 
                                                  database_host,
                                                  database_name)
-                                                 
+
 async def get_pool():
   return await aiopg.create_pool
 
 app = Sanic()
 
-@app.route("/models")
+@app.route("/models", methods=["GET",])
 async def handle(request):
   result = []
   async def test_select():
     async with aiopg.create_pool(connection) as pool:
       async with pool.acquire() as conn:
         async with conn.cursor() as cur:
-          await cur.execute('SELECT name, attribute, created_at FROM models')
+          await cur.execute('SELECT id, name, attribute, created_at FROM models')
           async for row in cur:
-            result.append({'name': row[0], 'attribute': row[1], 'created_at': row[2]})
+            result.append({'id': row[0], 'name': row[1], 'attribute': row[2], 'created_at': row[3]})
   res = await test_select()
   return json({'data': result})
   
-@app.route('/create_model', methods=['POST',])
-async def create_model(request):
-  print(request.body)
+@app.route('/models', methods=['POST',])
+async def handle(request):
   async with aiopg.create_pool(connection) as pool:
     async with pool.acquire() as conn:
       async with conn.cursor() as cur:
         await cur.execute("INSERT INTO models(name, attribute) VALUES(%s, %s)", (request.form.get('name'), request.form.get('attribute')))  
-  return json({'received': True, 'form_data': request.form, 'name': request.form.get('name'), 'attribute': request.form.get('attribute')})
+  return json({'received': True, 'form_data': request.form})
+  
+@app.route('/models/<id>', methods=["GET"])
+async def handle(request, id):
+  result = []
+  async def select():
+    async with aiopg.create_pool(connection) as pool:
+      async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+          await cur.execute('SELECT id, name, attribute, created_at FROM models WHERE id=' + id)
+          async for row in cur:
+            result.append({'id': row[0], 'name': row[1], 'attribute': row[2], 'created_at': row[3]})
+  res = await select()
+  return json({'data': result})
+  
+@app.route('/models/<id>', methods=['PUT',])
+async def handle(request, id):
+  async with aiopg.create_pool(connection) as pool:
+    async with pool.acquire() as conn:
+      async with conn.cursor() as cur:
+        await cur.execute("UPDATE models SET name=%s, attribute=%s WHERE id=%s", (request.form.get('name'), request.form.get('attribute'), id))  
+  return json({'received': True, 'form_data': request.form})  
+  
+@app.route('/models/<id>', methods=['DELETE',])
+async def handle(request, id):
+  async with aiopg.create_pool(connection) as pool:
+    async with pool.acquire() as conn:
+      async with conn.cursor() as cur:
+        await cur.execute("DELETE FROM models where id = " + id)  
+  return json({'received': True})
   
 # SSL= os.environ['SSL']
 
 # context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
 
-# context.load_cert_chain(SSL + '/fullchain.pem', keyfile=SSL + '/privkey.pem')  
+# context.load_cert_chain(SSL + '/fullchain.pem', keyfile=SSL + '/privkey.pem')
+
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=8000, debug=True)
+  app.run(host="localhost", port=8000, debug=True)
